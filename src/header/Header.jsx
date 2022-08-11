748bdb71e3e3134470f9e5bf8ead0de579a32c5b
John Xander <johnblox5@gmail.com>
2022-08-11 23:31:42 +0100

    Homepage

---
+++ src/header/Header.jsx
@@ -0,0 +1,16 @@
+import "./header.css"
+import viewImg from "../images/view.avif"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-titles">
+                <span className="header-title-sm">React & Node</span>
+                <span className="header-title-lg">Blog</span>
+            </div>
+            <img className="header-img" src={viewImg} alt="Turkey" />
+        </div>
+    )
+}
+
+export default Header
