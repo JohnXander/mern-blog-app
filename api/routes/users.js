a429d5c45e433ca75086790f13bdc8067ea520a8
John Xander <johnblox5@gmail.com>
2022-08-15 15:23:27 +0100

    Delete user from MongoDB

--- api/routes/users.js
+++ api/routes/users.js
@@ -23,26 +23,25 @@ router.put("/:id", async (req, res) => {
     }
 })
 
-//DELETE (BUG!!!)
-// router.delete("/:id", async (req, res) => {
-//     if (req.body.userId === req.params.id) {
-//         try {
-//             const user = await User.findbyId(req.params.id)
-//             console.log("helloooo", user)
-//             try {
-//                 await Post.deleteMany({ username: user.username })
-//                 await User.findByIdAndDelete(req.params.id)
-//                 res.status(200).json("User has been deleted!")
-//             } catch (err) {
-//                 res.status(500).json(err)
-//             }
-//         } catch (err) {
-//             res.status(404).json("User not found!")
-//         }
-//     } else {
-//         res.status(401).json("You can only delete your own account!")
-//     }
-// })
+// DELETE
 
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        try {
+            const user = await User.findById(req.params.id)
+            try {
+                await Post.deleteMany({ username: user.username})
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json("User has been deleted...")
+            } catch (err) {
+                res.status(500).json(err)
+            }
+        } catch (err) {
+            res.status(404).json("User not found!")
+        }
+    } else {
+        res.status(401).json("You can only delete your own account!")
+    }
+})
 
 module.exports = router
